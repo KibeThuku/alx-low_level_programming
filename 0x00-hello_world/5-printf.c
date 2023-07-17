fad7107654f311c2f0626e61e4659f8f61ad97b6
KibeThuku <kibethuku@gmail.com>
2023-07-17 10:18:21 -0700

    101-quote.c commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /*
- * main - Entry point
- *
- * Return: 0 always
- */
+* main  - this is the main function
+*
+* Return: 0 always
+*/
 
-int main()
+int main(void)
+	/* main - entry point */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);

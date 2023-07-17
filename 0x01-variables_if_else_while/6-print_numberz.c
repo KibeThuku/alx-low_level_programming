8d6685ff5a6b4d5aaa7c7abdcb89302224d68d43
KibeThuku <kibethuku@gmail.com>
2023-07-17 13:53:30 -0700

    6-print_numberz.c commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+/**
+ * main - main function
+ *
+ * Return: 0 always
+ */
+#include <stdio.h>
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}

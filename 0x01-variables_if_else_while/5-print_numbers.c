2c42b855a2d82c5f22ce684c4a79f115bbeb01ec
KibeThuku <kibethuku@gmail.com>
2023-07-17 13:45:56 -0700

    5-print_numbers.c commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * prints base 10 numbers starting
+ * with 0 followed by a newline
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	printf("\n");
+
+		return (0);
+}

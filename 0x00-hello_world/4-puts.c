c829b4e5fc019c026c1bc0c2bf7ef9f2989b5890
KibeThuku <kibethuku@gmail.com>
2023-07-17 08:41:35 -0700

    5-printf.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
+/*
+ * main - this is the main function
+ *
+ * return 0 always
+ */
 
+#include <stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
 /*
- *main - Entry point
+ * main - this is the main function
  *
- *Return: Always 0 (Success)
+ * return always 0
  */
+
+#include <stdio.h>
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");

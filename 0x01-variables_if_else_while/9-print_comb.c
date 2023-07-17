65f055f1db82523d284220c5603f22f9f66d0f56
KibeThuku <kibethuku@gmail.com>
2023-07-17 14:38:24 -0700

    9-print_comb.c commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+/**
+ * main - main function
+ *
+ * Return: 0 always
+ */
+#include <stdio.h>
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

8c53d2b96ec872720df7925b5c80b6f9f8b712de
KibeThuku <kibethuku@gmail.com>
2023-07-17 15:12:33 -0700

    101-print_comb4.c commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,22 +15,22 @@ int main(void)
 
         for (d = '0'; d < '9'; d++)
         {
-                for (p = d + 1; p <= '9'; p++)
-                {
-			for (q = d + 1; q <= '9'; q++)
-			{
-                        if ((p != d) != q)
-                        {
-                                putchar(d);
-                                putchar(p);
-				putchar(q);
-
-                                if (d == '7' && p == '8')
-                                        continue;
-                                putchar(',');
-                                putchar(' ');
-                        }
-                }
+        for (p = d + 1; p <= '9'; p++)
+        {
+	for (q = d + 1; q <= '9'; q++)
+	{
+        if ((p != d) != q)
+        {
+        putchar(d);
+        putchar(p);
+	putchar(q);
+        
+        if (d == '7' && p == '8')
+        continue;
+        putchar(',');
+        putchar(' ');
+        }
+        }
         }
 	}
         putchar('\n');

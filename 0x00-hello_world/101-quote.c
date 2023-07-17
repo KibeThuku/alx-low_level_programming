0b55bee54cea00f8b122ad774682242edc8c78a9
KibeThuku <kibethuku@gmail.com>
2023-07-17 12:48:53 -0700

    101-quote.c commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*
-*main - this is the main function body
-*
-*Return: Always 1 (Success)
-*/
+/**
+ * main - main function
+ *
+ * Return: 0 always
+ */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);

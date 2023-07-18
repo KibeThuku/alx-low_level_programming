6a9ab0c1ad58c6ec764ce9a6d732fe014b768e88
KibeThuku <kibethuku@gmail.com>
2023-07-18 08:28:01 -0700

    0-putchar.c commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,12 +5,19 @@
  * only use putchar to print any required info
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
+#include <unistd.h>
 
 int main(void)
 {
-	putchar("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }

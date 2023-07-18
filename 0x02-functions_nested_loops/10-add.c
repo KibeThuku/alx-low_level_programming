5b363284c11146435f3ba565fc24dd2239337a94
KibeThuku <kibethuku@gmail.com>
2023-07-18 12:58:37 -0700

    10-add.c commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,10 @@
 /**
- * add - adds two integers
+ * add-adds two integers
  * @x: number being added
  * @y: number being added
  * return: 0 always
  */
+#include "main.h"
 int add(int x, int y)
 {
 	return (x + y);

c2253de8b671b2b10397f741738ca9bd68633d24
KibeThuku <kibethuku@gmail.com>
2023-07-18 10:05:34 -0700

    2-print_alphabet_x10.c commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+/**
+ * void print_alphabet_x10(void)-main function
+ * print 10 times the alphabet in lowercase
+ * only use _putchar
+ * return: 0 always
+ */
+#include "main.h"
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	i = 0;
+	while(i < 10)
+	{
+		letter = 'a';
+		while (letter <='z')
+		{
+			_putchar(letter);
+				letter++;
+		}
+		_putchar('\n');
+			i++;
+	}
+}

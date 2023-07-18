8539b988da291c32069e7e20368db455cf8aa77b
KibeThuku <kibethuku@gmail.com>
2023-07-18 07:28:08 -0700

    0-putchar.c commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+/**
+ * main - main function
+ *
+ * Return: always 0
+ * only use putchar to print any required info
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	putchar("_putchar\n");
+	return (0);
+}

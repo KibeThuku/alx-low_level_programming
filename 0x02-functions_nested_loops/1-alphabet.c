c032fca6a1da68b114f9a92daf15cd446b008196
KibeThuku <kibethuku@gmail.com>
2023-07-18 09:13:10 -0700

    1-alphabet.c commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
-
-/* print_alphabet-main function
- * print alphabet in lower case
+/**
+ * print_alphabet-main function
  * return: 0 always
+ * print lowercase alphabets upto z
  */
 #include "main.h"
 void print_alphabet(void)

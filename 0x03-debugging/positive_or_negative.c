82dc38d04e48cc4b7f74a0f5a3474b58da833c2a
KibeThuku <kibethuku@gmail.com>
2023-07-21 06:29:43 -0700

    0x03-debugging commit

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - main function
+ *
+ * Return: 0
+ */
+
+positive_or_negative()
+{
+	if (n>0)
+		putchar("%d\n, is a positive number");
+	else if (n = 0)
+		putchar("%d\n, is zero);
+	else
+		putchar

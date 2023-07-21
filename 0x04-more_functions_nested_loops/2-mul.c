1b44829038af703b4588ae38d5864e1c187e8def
KibeThuku <kibethuku@gmail.com>
2023-07-21 16:32:53 -0700

    2-mul.c commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - main function multiplying integers
+ *
+ * @a: input 1st integer
+ * @b: input 2nd integer
+ *
+ * Return: 0 always
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

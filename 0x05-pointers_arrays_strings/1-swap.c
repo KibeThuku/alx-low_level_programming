dd297a6069ee1406ee0b2b3ffe45752d4a4ebdb6
KibeThuku <kibethuku@gmail.com>
2023-07-24 04:30:45 -0700

    1-swap.c commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - function is used to swap numbers
+ *
+ * @a: input integer
+ * @b: input integer
+ *
+ * Return: 0 always
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}

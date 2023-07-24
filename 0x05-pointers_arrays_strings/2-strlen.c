751cf11bde65089bbae0209ecd916fc2b1c4ff23
KibeThuku <kibethuku@gmail.com>
2023-07-24 05:19:20 -0700

    2-strlen.c commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,12 +7,12 @@
  * Return: length of the string
  */
 
-int _strlen(char *s);
+int _strlen(char *s)
 {
 	int i;
 	int count = 0;
 
-	for (i = 0; s(i) != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 		count++;
 	return (count);
 }

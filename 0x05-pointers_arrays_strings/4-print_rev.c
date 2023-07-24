c91492c9dea85ab967e7d9192e9a52de3ab66aa8
KibeThuku <kibethuku@gmail.com>
2023-07-24 06:21:21 -0700

    4-print_rev.c commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - reverses the string characters
+ * @s:input
+ * Return: string characters
+ */
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != 0; i++)
+		count++;
+	for (i = count ; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}

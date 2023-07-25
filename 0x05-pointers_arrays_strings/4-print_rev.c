bf295508100dd0ae2ba3d80aa524230b64f06340
KibeThuku <kibethuku@gmail.com>
2023-07-25 02:50:49 -0700

    4-print_rev.c commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_rev - reverses the string characters
- * @s:input
- * Return: string characters
+ * print_rev - prints string characters in reverse
+ * @s: string input
+ * Return: string in reverse
  */
+
 void print_rev(char *s)
 {
 	int i;
-	int count = 0;
+	int sleng = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-		count++;
-	for (i = count ; i >= 0; i--)
-		_putchar(s[i]);
+	while (*s != '\0')
+	{
+		sleng++;
+		s++;
+	}
 	s--;
+	for (i = sleng; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
 	_putchar('\n');
 }

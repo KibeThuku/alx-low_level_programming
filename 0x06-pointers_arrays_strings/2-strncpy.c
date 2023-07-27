04ec8d1dec59bc777f4b584eeaa4dc941c3a5678
KibeThuku <kibethuku@gmail.com>
2023-07-27 02:31:20 -0700

    2-strncpy. c commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings
+ * _strncpy - copies a strings
  * @dest: destination streng
  * @src: source string
  * @n: maximum number of bytes to copied from src
@@ -9,13 +9,14 @@
  * Return: dest
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int index = 0;
 	int src_len = 0;
 
 	while (src[index++])
 		src_len++;
+
 	for (index = 0;  src[index] && index < n; index++)
 		dest[index] = src[index];
 

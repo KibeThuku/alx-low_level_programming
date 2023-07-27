dc2cf80170c365b6c18b442c21feced1c848fb6b
KibeThuku <kibethuku@gmail.com>
2023-07-27 02:37:15 -0700

    1-strncat.c commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,15 +12,11 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0;
-	int src_len = 0;
+	int dest_len = 0;
 
-	while (src[index++])
-		src_len++;
-	for (index = 0;  src[index] && index < n; index++)
-		dest[index] = src[index];
-
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
-
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }

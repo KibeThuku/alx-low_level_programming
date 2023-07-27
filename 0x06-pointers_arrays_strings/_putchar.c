daa850a35e0e040c2e17c8d392d90c8bb68c3864
KibeThuku <kibethuku@gmail.com>
2023-07-27 01:45:30 -0700

    0-strcat commit

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _putchar - main function
+ *
+ * @c: the character printed out
+ * Return: 0 on success -1 on fail
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}

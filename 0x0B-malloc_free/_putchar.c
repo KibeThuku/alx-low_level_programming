b7f0cd78f548a7dd87fe062faa545776be42cc04
KibeThuku <kibethuku@gmail.com>
2023-08-08 18:40:47 -0700

    0x0B-malloc_free commit

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _putchar - printing function
+ *
+ * @c: the character printed out
+ * Return: 0 on success -1 on failure
+ */
+
+int _putchar(char c)
+{
+	return (write(1, $c, 1));
+}

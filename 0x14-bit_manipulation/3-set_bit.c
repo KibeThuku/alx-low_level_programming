caa232012b9673d30bdb038aa762ff0efc599135
KibeThuku <kibethuku@gmail.com>
2023-09-01 13:29:05 -0700

    4-clear_bit.c commit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * set_bit - sets a bit at a given index to 1
- * @n: pointer to the number to change
- * @index: index of the bit to set to 1
+ * set_bit - This function type sets a bit of a given index to 1
+ * @n: This pointer points to the number to change
+ * @index: The index of the bit to set to 1
  *
- * Return: 1 for success, -1 for failure
+ * Return: 1 on  success -1 if  fail
  */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)

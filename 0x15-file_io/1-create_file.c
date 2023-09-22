c5c691421de12536b4fb29ab08e1be0183ea1998
KibeThuku <kibethuku@gmail.com>
2023-09-21 19:59:17 -0700

    2-append_text_to_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include main.h
+
+/**
+ * TASK 1. UNDER THE SNOW - 1-create_file.c
+ *
+ * @create_file - integer function creating a file.
+ * (const char *filename, char *text_content) - Function arguement. 
+ * @filename: Pointer pointing to a character file to be created.
+ * @text_content - pointer pointing to string characters 
+ * to be written to the file.
+ * Return:1 on success, -1 on failure 
+ * (file can not be created, file can not be written, write “fails”, etc)
+ */
+
+include create_file(const char *filename, char *text_content)
+{
+	int fdes;
+	int let;
+	int leng = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (leng = 0; text_conten[leng];)
+			len++;
+	}
+
+	fdes = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	let = write((fdes, text_content, leng);
+
+			if (fdes == -1 || let == -1)
+			return (-1);
+
+			close(fdes);
+
+			return (1);
+}
